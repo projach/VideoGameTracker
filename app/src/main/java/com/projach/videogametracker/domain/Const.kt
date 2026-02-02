55edbb237f0229ba6a91af9ba44ac08a6d482354
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2026-02-03 00:17:50 +0200

    Add constants

---
+++ app/src/main/java/com/projach/videogametracker/domain/Const.kt
@@ -0,0 +1,8 @@
+package com.projach.videogametracker.domain
+
+const val IGDB_BASE_URL = "https://api.igdb.com/v4/"
+const val TWIRCH_BASE_URL = "https://id.twitch.tv/"
+const val POST_VIDEO_GAMES = "games"
+const val POST_TWITCH_TOKEN = "oauth2/token"
+const val PAGE_LIMIT = 20
+const val GRANT_TYPE = "client_credentials"
