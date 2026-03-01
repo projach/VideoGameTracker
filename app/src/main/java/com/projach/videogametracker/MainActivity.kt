cac787d0df1b0cd6ab84f0ee2410d16c6a9654c9
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2026-03-01 13:36:57 +0200

    Fix merge conflict

--- app/src/main/java/com/projach/videogametracker/MainActivity.kt
+++ app/src/main/java/com/projach/videogametracker/MainActivity.kt
@@ -3,15 +3,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.Text
 import com.projach.videogametracker.ui.theme.VideoGameTrackerTheme
-import com.projach.videogametracker.ui.videoGamesList.VideoGamesListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
