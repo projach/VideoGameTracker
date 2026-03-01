fe6efbcd5e23cbbc9e1abe332b87452d53ed1a58
projach <ichaliasas@gmail.com>
2026-03-01 02:37:34 +0200

    Update ksp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
 }
